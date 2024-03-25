10a1141bdb2bc82059730fe9a592d0d8ce6f5d5e
Rick Conlee <rick.conlee@gmail.com>
2024-03-25 11:46:34 -0700

    Made a goof on the NAT adapter. Issue #13

--- virtualbox-create.sh
+++ virtualbox-create.sh
@@ -5,6 +5,6 @@ if VBoxManage list natnetworks | grep -q "LabNAT"; then
     echo "LabNAT exists."
 else
     echo "LabNAT does not exist. Creating it..."
-    VBoxManage natnetwork add --netname LabNAT --network "10.230.10.1/24" --enable --dhcp off
-    echo "LabNAT created with subnet 10.230.10.1/24 without DHCP server."
+    VBoxManage natnetwork add --netname LabNAT --network "10.230.10.0/24" --enable --dhcp off
+    echo "LabNAT created with subnet 10.230.10.0/24 without DHCP server."
 fi
