ea7f429cfb96f7a71105d5a0ba3ea2e09738e349
Rick Conlee <rick.conlee@gmail.com>
2024-03-25 10:46:28 -0700

    Created bootstrapping script, works so fat. Part of issue #13

---
+++ virtualbox-create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if "LabNAT" exists
+if VBoxManage list natnetworks | grep -q "LabNAT"; then
+    echo "LabNAT exists."
+else
+    echo "LabNAT does not exist. Creating it..."
+    VBoxManage natnetwork add --netname LabNAT --network "10.230.10.1/24" --enable --dhcp off
+    echo "LabNAT created with subnet 10.230.10.1/24 without DHCP server."
+fi
